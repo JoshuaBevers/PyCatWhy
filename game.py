8a07c0d968d922e28fffd92eaf0036412c78ba16
infernezor@gmail.com <infernezor@gmail.com>
2020-01-23 18:26:13 -0500

    Fixed cat anger.

--- game.py
+++ game.py
@@ -9,6 +9,7 @@ from init.gameinitializers import *
 from objects.cat import *
 from objects.click_box import *
 from objects.background import *
+from objects.cat_carrier import *
 
 # pygame.mixer.pre_init(44100, 16, 2, 4096)
 # Initialize pygame and create window
@@ -31,6 +32,8 @@ pygame.time.set_timer(spawn_orange + 1, SPAWN_SPEED)
 # Space allocated for creating and adding variables.
 bg = Background()
 cat = Cat()
+goal = Carrier(BLACK, 100, 80)
+
 orange = Orange(GREEN, 40, 40)
 
 top_left = Click_Box('top_left')
@@ -44,6 +47,7 @@ all_sprites = pygame.sprite.Group()
 click_boxes = pygame.sprite.Group()
 obsticals = pygame.sprite.Group()
 all_sprites.add(cat)
+all_sprites.add(goal)
 click_boxes.add(top_left, top_right, bottom_left, bottom_right)
 
 
@@ -51,6 +55,8 @@ click_boxes.add(top_left, top_right, bottom_left, bottom_right)
 bg.start_music()
 
 # Create functions
+
+
 def cattitude(surf, x, y, pct):
     if pct < 0:
         pct = 0
@@ -61,6 +67,7 @@ def cattitude(surf, x, y, pct):
     fill_rect = pygame.Rect(x, y, fill, BAR_HEIGHT)
     pygame.draw.rect(surf, RED, fill_rect)
     pygame.draw.rect(surf, BLACK, outline_rect, 2)
+
 
 def create_orange():
     all_sprites.add(orange)
@@ -96,6 +103,7 @@ while running:
             if top_left.rect.collidepoint(x, y):
                 cat.change_direction('top_left')
                 if attitude == 1:
+                    cat.anger += 5
                     print("This cat has attitude!")
                     changeDir = random.randint(1, 3)
                     if changeDir == 1:
@@ -107,6 +115,7 @@ while running:
             if top_right.rect.collidepoint(x, y):
                 cat.change_direction('top_right')
                 if attitude == 1:
+                    cat.anger += 5
                     print("This cat has attitude!")
                     changeDir = random.randint(1, 3)
                     if changeDir == 1:
@@ -118,6 +127,7 @@ while running:
             if bottom_left.rect.collidepoint(x, y):
                 cat.change_direction('bottom_left')
                 if attitude == 1:
+                    cat.anger += 5
                     print("This cat has attitude!")
                     changeDir = random.randint(1, 3)
                     if changeDir == 1:
@@ -129,6 +139,7 @@ while running:
             if bottom_right.rect.collidepoint(x, y):
                 cat.change_direction('bottom_right')
                 if attitude == 1:
+                    cat.anger += 5
                     print("This cat has attitude!")
                     changeDir = random.randint(1, 3)
                     if changeDir == 1:
