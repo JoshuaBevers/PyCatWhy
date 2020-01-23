f46c2c1f01eabeb2eb5a719685b97dd38a28398b
Karley Petracca <karleypetracca@gmail.com>
2020-01-23 11:17:57 -0500

    Added mouse position tracking, key import to gameinitializers file and file name change under init

--- init/gameinitalizers.py
+++ init/gameinitalizers.py
@@ -1,3 +1,5 @@
+import pygame
+
 # define colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -5,8 +7,18 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-
+# define game screen sizing and fps
 WIDTH = 780
 HEIGHT = 780
 FPS = 30
 
+# Import pygame.locals to establish necessary key commands
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
