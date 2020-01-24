cc6125900dc224d45133a11f0b7983c26595a129
Zach Barbre <zach.barbre@gmail.com>
2020-01-24 11:13:14 -0500

    complete title screen

--- game.py
+++ game.py
@@ -10,10 +10,7 @@ from objects.click_box import *
 from objects.background import *
 from objects.cat_carrier import *
 from objects.cat import *
-<<<<<<< HEAD
 from objects.menu import *
-=======
->>>>>>> 0db23854d19fe0006e1032e5fa5f8e75bb91aabe
 
 pygame.init()
 pygame.mixer.init(44100, -16, 2, 2048)
@@ -36,10 +33,7 @@ bg = Background()
 cat = Cat()
 goal = Carrier(BLACK, 100, 80)
 orange = Orange(POWER, 40, 40)
-<<<<<<< HEAD
-menu = Menu('images/title_screen.png')
-=======
->>>>>>> 0db23854d19fe0006e1032e5fa5f8e75bb91aabe
+menu = Menu('images/title-scren.png')
 
 top_left = Click_Box('top_left')
 top_right = Click_Box('top_right')
@@ -97,33 +91,9 @@ def cattitude(surf, x, y, pct):
 def create_orange():
     all_sprites.add(orange)
     
-font_name = pygame.font.match_font('arial')
-
-def draw_text(surf, text, size, x, y):
-    font = pygame.font.Font(font_name, size)
-    text_surface = font.render(text, True, WHITE)
-    text_rect = text_surface.get_rect()
-    text_rect.midtop = (x, y)
-    surf.blit(text_surface, text_rect)
-
-def show_menu_screen():
-    screen.blit(menu.menu_image, menu.rect)
-    # draw_text(screen, "Get the Cat to the Carrier", 64, WIDTH / 2, HEIGHT / 4)
-    pygame.display.flip()
-    waiting = True
-    while waiting:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            # Any key press will start the game
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                waiting = False
-
-
 ### Game loop ###
 running = True
-menu_scren = True
+menu_screen = True
 
 while running:
     # variables being kept track of at the start of the game.
@@ -131,9 +101,9 @@ while running:
     # Keep loop running at the right speed
     clock.tick(FPS)
     # Process input (events)
-    if menu:
-        show_menu_screen()
-        menu_scren = False
+    if menu_screen:
+        menu.show_menu_screen(screen, clock)
+        menu_screen = False
 
     for event in pygame.event.get():
         if event.type == spawn_orange + 1:
