2e8d48ecf97ccef02b3a82452cf5c69b757acd9b
Zach Barbre <zach.barbre@gmail.com>
2020-01-24 12:53:34 -0500

    readabilty edits in menu.py

--- objects/menu.py
+++ objects/menu.py
@@ -5,11 +5,24 @@ from init.gameinitializers import *
 class Menu(pygame.sprite.Sprite):
     def __init__(self, menu_image):
         pygame.sprite.Sprite.__init__(self)
-        self.menu_image = menu_image
-        self.background = pygame.image.load(self.menu_image)
+        self.background = pygame.image.load(menu_image)
         self.rect = self.background.get_rect()
-        self.rect.left, self.rect.top = (0, 0)
+        self.rect.topleft = (0, 0)
         
+    def show_menu_screen(self, screen, clock):
+        screen.blit(self.background, self.rect)
+        pygame.display.flip()
+        waiting = True
+        while waiting:
+            clock.tick(FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                # click the mouse to start
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    waiting = False
+
+
     def draw_text(surf, text, size, x, y):
         font_name = pygame.font.match_font('arial')
 
@@ -18,17 +31,3 @@ class Menu(pygame.sprite.Sprite):
         text_rect = text_surface.get_rect()
         text_rect.midtop = (x, y)
         surf.blit(text_surface, text_rect)
-
-    def show_menu_screen(self, screen, clock):
-        screen.blit(self.background, self.rect)
-        # draw_text(screen, "Get the Cat to the Carrier", 64, WIDTH / 2, HEIGHT / 4)
-        pygame.display.flip()
-        waiting = True
-        while waiting:
-            clock.tick(FPS)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                # Any key press will start the game
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    waiting = False
